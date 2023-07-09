707fd9e037c7065ca161e79ffbfdcd100348dc85
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-07-09 12:08:48 +0300

    Create Print Prime Numbers.sql

---
+++ Solutions/Print Prime Numbers.sql
@@ -0,0 +1,22 @@
+DECLARE @I INT=2
+DECLARE @PRIME INT=0
+DECLARE @OUTPUT TABLE (NUM INT)
+WHILE @I<=1000
+BEGIN
+    DECLARE @J INT = 2
+    SET @PRIME=1
+    WHILE @J<(sqrt(@I)+1)
+    BEGIN
+        IF @I % @J=0 
+        BEGIN
+            SET @PRIME=0
+        END
+        SET @J=@J+1
+    END
+    IF @PRIME =1 OR @I=2
+    BEGIN
+        INSERT @OUTPUT VALUES (@I)
+    END
+    SET @I=@I+1
+END
+SELECT string_agg(CAST(NUM AS VARCHAR), '&') FROM @OUTPUT
