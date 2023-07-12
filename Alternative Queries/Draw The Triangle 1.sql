041ee8a5af23362b96a05ad9a0c110315332bd84
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-07-12 16:39:49 +0300

    Create Draw The Triangle 1.sql

---
+++ Alternative Queries/Draw The Triangle 1.sql
@@ -0,0 +1,18 @@
+set serveroutput on;
+DECLARE
+I NUMBER; -- redundant
+J NUMBER; -- redundant
+BEGIN
+FOR I IN REVERSE 1..20
+LOOP
+    FOR J IN 1..I
+    LOOP
+    DBMS_OUTPUT.PUT('*') ; -- printing *
+    IF J < I THEN
+        DBMS_OUTPUT.PUT(' ');
+    END IF;
+    END LOOP;
+    DBMS_OUTPUT.NEW_LINE; -- for new line
+END LOOP;
+END;
+/
