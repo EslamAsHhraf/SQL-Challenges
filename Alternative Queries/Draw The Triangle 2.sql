58c6a9701336a1bf0a1348f0abcd46f5517088da
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-07-12 16:41:55 +0300

    Create Draw The Triangle 2.sql

---
+++ Alternative Queries/Draw The Triangle 2.sql
@@ -0,0 +1,21 @@
+/* 
+Working platform:- Oracle
+*/
+set serveroutput on;
+DECLARE
+I NUMBER; -- redundant
+J NUMBER; -- redundant
+BEGIN
+FOR I IN 1..20
+LOOP
+    FOR J IN 1..I
+    LOOP
+    DBMS_OUTPUT.PUT('*') ; -- printing *
+    IF J < I THEN
+        DBMS_OUTPUT.PUT(' ');
+    END IF;
+    END LOOP;
+    DBMS_OUTPUT.NEW_LINE; -- for new line
+END LOOP;
+END;
+/
